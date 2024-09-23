5e7c79e283ce2ac1361b5b5880ce18c8cc71db84
Alona_a <58535834+Alonalos@users.noreply.github.com>
2024-09-23 19:17:26 -0400

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo apt install php libapache2-mod-php php-mysql -y
+cd /var/www/html
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xzf latest.tar.gz
+sudo rm -rf latest.tar.gz
+sudo mv wordpress/* .
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
+sudo systemctl restart apache2
