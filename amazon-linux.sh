219f19a63bf0243d58ef7a2e60c0ffb619942438
Alona_a <58535834+Alonalos@users.noreply.github.com>
2024-09-23 19:01:10 -0400

    Update amazon-linux.sh

--- amazon-linux.sh
+++ amazon-linux.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-sudo yum update -y || sudo apt update -y
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo amazon-linux-extras install php7.4 -y
+sudo yum install php php-mysqlnd php-fpm -y
+cd /var/www/html
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xzf latest.tar.gz
+sudo rm -rf latest.tar.gz
+sudo mv wordpress/* .
+sudo chown -R apache:apache /var/www/html
+sudo chmod -R 755 /var/www/html
 
-echo "Installing Apache..."
-sudo yum install httpd -y || sudo apt install apache2 -y
-sudo systemctl start httpd || sudo systemctl start apache2
-sudo systemctl enable httpd || sudo systemctl enable apache2
+# Restart Apache to apply changes
+sudo systemctl restart httpd
