83f2019c4138408852faec5b76b450d8330cfa0f
EC2 Default User <ec2-user@ip-172-31-4-197.ec2.internal>
2024-09-24 03:37:04 +0000

    Homework12 age and calculator

---
+++ Homework12/age.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "Please input your age " age
+current_year=2024
+birth_year=$((current_year-age))
+echo "In 2020 you were $((age-4))"
+echo "In 2030 you will be $((age+6))"
---
+++ Homework12/calculator.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "Enter 2 numbers: " m n
+echo "Sum = $((m + n))"
+echo "Difference = $((m - n))"
+echo "Product = $((m * n))"
+echo "Quotient = $((m / n))"
